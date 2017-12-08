d22cd3eee9549773cb01e8bdf9aa7abd668e108a
BlackSpirit03 <d.leger@aformac-vichy.fr>
2017-12-08 16:59:50 +0100

    update
    update

--- site_deveandcie/app/Http/Controllers/ConfirmController.php
+++ site_deveandcie/app/Http/Controllers/ConfirmController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Routing\Controller;
 use App\models\Transfers;
 
 class ConfirmController extends Controller
@@ -25,8 +26,8 @@ class ConfirmController extends Controller
         $transfer = Transfers::find($id_transfer);
         
         //procéder au téléchargement du fichier
-        return Storage::disk('upload')->download($transfer->file_name,$transfer->real_name);
-        dd('fin');
+        return response()->Storage::disk('upload')->download($transfer->file_name,$transfer->real_name);
+        
 
         //affichage du résultat
         return view('confirmdl');
