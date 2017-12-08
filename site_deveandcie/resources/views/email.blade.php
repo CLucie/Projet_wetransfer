f7e0288cce306516b7b513f4bb404b83686ccb83
BlackSpirit03 <d.leger@aformac-vichy.fr>
2017-12-08 13:50:22 +0100

    update with token for download
    update with token for download

--- site_deveandcie/resources/views/email.blade.php
+++ site_deveandcie/resources/views/email.blade.php
@@ -10,7 +10,7 @@
       <li><strong>Expéditeur</strong> : {{ $contact->exp_mail }}</li>
       <li><strong>Destinataire</strong> : {{ $contact->dest_mail }}</li>
       <li><strong>Message</strong> : {{ $contact->dest_message }}</li>
-      <li><strong>Lien de téléchargement</strong> : <a href="{{ route('download',['id_transfer'=>$contact->id_transfer]) }}">{{$contact->real_name}}</a></li>
+      <li><strong>Lien de téléchargement</strong> : <a href="{{ route('download',['by_token'=>$contact->file_name]) }}">{{$contact->real_name}}</a></li>
     </ul>
   </body>
 </html>
